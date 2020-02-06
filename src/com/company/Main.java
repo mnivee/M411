1270032fa503659c36b4d00e8f69fc9078448200
Niveditha <niveditha_1999@hotmail.com>
2020-02-06 11:22:53 +0100

    did smth

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -27,6 +27,17 @@ public class Main {
         Long n = Long.parseLong(eingabe);
         System.out.println("Collatz-Berechnung für: " + eingabe);
         collatzBerechnung(n);
+
+        // Matrix anzeigen und auslesen
+
+        int [][] matrix = {{4, 5, 6},
+                {2, -9, -3}};
+        for(int i =0; i<matrix.length; i++){
+            for(int j=0; j<matrix[i].length; j++){
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 
 
